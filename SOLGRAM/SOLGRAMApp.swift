d2e2fa7d4ff24f436adcc23d0640a5bcc0ff338a
fobass <isabekov.ernist@n2nconnect.com>
2023-04-27 20:33:12 +0600

    Initial Commit

---
+++ SOLGRAM/SOLGRAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOLGRAMApp.swift
+//  SOLGRAM
+//
+//  Created by Ernist Isabekov on 27/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SOLGRAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
